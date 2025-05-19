3fcc4cfe04ebdfa2b360ca9d7386f4fcc6fb7d8e
XDZltero <128982667+XDZltero@users.noreply.github.com>
2025-05-19 10:34:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
-from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins=["https://xdzltero.github.io"])
 
-# 從環境變數載入 JSON 字串並解析成 Python dict
+# Firebase 初始化
 firebase_creds_str = os.environ["FIREBASE_CREDENTIALS"]
 firebase_creds = json.loads(firebase_creds_str)
-
-# 初始化 Firebase Admin
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
